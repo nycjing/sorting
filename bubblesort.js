1f2720f671d23c49c43c0f57da3e5ebb8b2534b4
Erik Shuttlesworth <erik.shuttlesworth@gmail.com>
2017-07-27 11:54:58 -0500

    finished bubbleSort

--- bubblesort.js
+++ bubblesort.js
@@ -1,14 +1,23 @@
 function bubbleSort(array) {
+    var swapCount = 1;
+    var roundCount = 0;
 
-    /* your code here */
-    var result =[];
+    while (swapCount) {
+        swapCount = 0;
+        for (var i = 0; i < array.length - roundCount; i++) {
+            if (array[i] > array[i + 1]) {
+                swapCount++;
+                var temp = swap(array[i], array[i + 1]);
+                array[i] = temp[0];
+                array[i + 1] = temp[1];
+            }
+        }
+        roundCount++;
+    }
 
-    return result
-
-
+    return array;
 }
 
-function swap (a,b){
-
-
+function swap(a, b) {
+    return [b, a];
 }
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -3,10 +3,17 @@ describe('Bubble Sort', function(){
         expect( bubbleSort([]) ).toEqual( [] );
     });
 
-    // expect(swapCounter).toEqual(100).
+    // expect(swapCounter).toEqual(100)
 
     it('getting to call swap', function () {
-        spyOn(swap()).and.callThrough();
+        spyOn(window, 'swap').and.callThrough();
         expect(swap.calls.count()).toEqual(0);
+    });
+
+    it('sorts correctly', function() {
+        spyOn(window, 'swap').and.callThrough();
+        var unsorted = [5, 8, 2, 4, 12, 1, 3];
+        expect( bubbleSort(unsorted) ).toEqual( [1, 2, 3, 4, 5, 8, 12] );
+        console.log(swap.calls.count());
     });
 });
